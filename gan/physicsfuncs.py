4af8cb725089d4b8c7922095118f689111a06ed9
Bobak Hashemi <bth5032@gmail.com>
2017-10-30 16:26:27 +0100

    ruined everything with numpy garbage

--- gan/physicsfuncs.py
+++ gan/physicsfuncs.py
@@ -1,23 +1,18 @@
-import math
+import numpy as np
 
-def M_ptetaphi(mom):
+def M_ptetaphi(E1, pt1, eta1, phi1, E2, pt2, eta2, phi2):
   """Outputs the Mass of a two particle system when given a list of E1, pt1, eta1, phi1, E2, pt2, eta2, phi2"""
-  E1, pt1, eta1, phi1, E2, pt2, eta2, phi2 = map(float, mom)
-  p1x = pt1*math.cos(phi1)
-  p1y = pt1*math.sin(phi1)
-  p1z = pt1*math.sinh(eta1)
+  p1x = pt1*np.cos(phi1)
+  p1y = pt1*np.sin(phi1)
+  p1z = pt1*np.sinh(eta1)
 
-  p2x = pt2*math.cos(phi2)
-  p2y = pt2*math.sin(phi2)
-  p2z = pt2*math.sinh(eta2)
+  p2x = pt2*np.cos(phi2)
+  p2y = pt2*np.sin(phi2)
+  p2z = pt2*np.sinh(eta2)
 
-  return M(E1, px1, py1, pz1, E2, px2, py2, pz2)
+  return M(E1, p1x, p1y, p1z, E2, p2x, p2y, p2z)
 
-def M_xyz(mom):
-  """Outputs the Mass of a two particle system when given a list of E1, px1, py1, pz1, E2, px2, py2, pz2"""
-  return M(*map(float, mom))
-
-def M(E1, px1, py1, pz1, E2, px2, py2, pz2):
+def M(E1, p1x, p1y, p1z, E2, p2x, p2y, p2z):
   """Computes M for two objects given the Carteasan momentum projections"""
   M = (E1+E2)**2 - (p1x+p2x)**2 - (p1y+p2y)**2 - (p1z+p2z)**2
-  return (str(math.sqrt(M)) if (M > 0) else None)
+  return np.sqrt(M)
