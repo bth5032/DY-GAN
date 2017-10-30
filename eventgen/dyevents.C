4310b3a9afea2a093702429b6699a5053508666a
Bobak Hashemi <bth5032@gmail.com>
2017-10-30 16:54:49 +0100

    event scaprer now dumps px,py,pz

--- eventgen/dyevents.C
+++ eventgen/dyevents.C
@@ -9,7 +9,8 @@ void dyevents()
   TChain *ch = new TChain("t");
   ch->Add("/nfs-7/userdata/bhashemi/WWW_babies/WWW_v0.1.16/output/dy_m50_mgmlm_ext1*");
   ch->SetScanField(0);
-  TString query = "genPart_pdgId:genPart_p4.E():genPart_p4.pt():genPart_p4.eta():genPart_p4.phi()";
+  //pt,eta,phi TString query = "genPart_pdgId:genPart_p4.E():genPart_p4.pt():genPart_p4.eta():genPart_p4.phi()";
+  TString query = "genPart_pdgId:genPart_p4.E():genPart_p4.X():genPart_p4.Y():genPart_p4.Z()";
   TString selection = Form("genPart_status==1 && Sum$(genPart_status==1 && abs(genPart_pdgId)==%i && abs(genPart_motherId) == 23 )==2 && (abs(genPart_pdgId)==%i && abs(genPart_motherId) == 23)", pdgId, pdgId);
   ch->GetEntries(selection);
   //ch->Scan(query , selection);
