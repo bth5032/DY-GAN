823b8fb04151dd055a909e392de594b4101cac06
Bobak Hashemi <bth5032@gmail.com>
2017-11-03 18:55:31 +0100

    directory standard now in place

--- gan/useGenerator.py
+++ gan/useGenerator.py
@@ -10,7 +10,7 @@ from keras.callbacks import TensorBoard
 
 from sklearn.model_selection import train_test_split
 
-import sys
+import sys, os, argparse
 sys.path.append("../")
 
 import plottery.plottery as ply
@@ -20,67 +20,83 @@ import matplotlib.pyplot as plt
 import ROOT as r
 from physicsfuncs import M
 
-import sys
-import os
-
 import numpy as np
 
-model_name=None
-ngen_samples=50000
-input_size=(23,)
-truth_data = np.loadtxt(open("dy_mm_events_line.input", "r"), delimiter=",", skiprows=1)
-truth_masses = truth_data[:,0]
-truth_4vecs = truth_data[:,range(1,1+8)]
 
-print(truth_masses)
-h2 = r.TH1F("h2","masses",60,60,120)
-#plu.fill_fast(h2, truthmasses)
-for val in truth_masses: h2.Fill(val)
-h2.Scale(1./h2.Integral())
+def generatePlots(model_dir, ngen_samples=50000):
+  input_size=(23,)
+  truth_data = np.loadtxt(open("dy_mm_events_line.input", "r"), delimiter=",", skiprows=1)
+  truth_masses = truth_data[:,0]
+  truth_4vecs = truth_data[:,range(1,1+8)]
 
-#Check for a model in model/ (a directory starting with model_)
-if (os.listdir("model/")):
-  model_name=filter(lambda x: "model_" in x, os.listdir("model/"))[0]
+  print(truth_masses)
+  h2 = r.TH1F("h2","masses",60,60,120)
+  #plu.fill_fast(h2, truthmasses)
+  for val in truth_masses: h2.Fill(val)
+  h2.Scale(1./h2.Integral())
 
-#If no directory exists, exit, no model can be found
-if not model_name:
-  print("No model found in model/, please copy one over from old_models or run gan.py to generate a model")
-  exit(1)
-else:
-  print("Making plots for model in directory: %s" % model_name)
 
-#The one liner is a bit ugly, but it just gets a list of full paths to the gen_*.weights files inside of model/model_<model_name>/
-#i.e. it finds all the models we want to run over...
-#for saved_model in ["model/model_TruthBiased1/gen_10000.weights", "model/model_TruthBiased1/gen_20000.weights"]: #map(lambda y: "model/"+model_name+"/"+y, filter(lambda x: ("gen_" in x and ".weights" in x), os.listdir("model/"+model_name))):
-for saved_model in map(lambda y: "model/"+model_name+"/"+y, filter(lambda x: ("gen_" in x and ".weights" in x), os.listdir("model/"+model_name))):
-  model = load_model(saved_model)
-  model.summary()
-  
-  noise = np.random.normal(0, 1, (ngen_samples,input_size[0]-truth_4vecs.shape[1]))
-  rand_rows = np.random.randint(0, truth_4vecs.shape[0], ngen_samples)
-  gen_input = np.c_[noise, truth_4vecs[rand_rows]]
-  print(gen_input.shape)
-  gen_output = model.predict(gen_input)
-  print(gen_output.shape)
-  masses=M(gen_output[:,0],gen_output[:,1],gen_output[:,2],gen_output[:,3],gen_output[:,4],gen_output[:,5],gen_output[:,6],gen_output[:,7])
+  #The one liner is a bit ugly, but it just gets a list of full paths to the gen_*.weights files inside of model/model_<model_name>/
+  #i.e. it finds all the models we want to run over...
+  #for saved_model in ["model/model_TruthBiased1/gen_10000.weights", "model/model_TruthBiased1/gen_20000.weights"]: #map(lambda y: "model/"+model_name+"/"+y, filter(lambda x: ("gen_" in x and ".weights" in x), os.listdir("model/"+model_name))):
+  for saved_model in map(lambda y: model_dir+y, filter(lambda x: ("gen_" in x and ".weights" in x), os.listdir(model_dir))):
+    model = load_model(saved_model)
+    model.summary()
+    
+    noise = np.random.normal(0, 1, (ngen_samples,input_size[0]-truth_4vecs.shape[1]))
+    rand_rows = np.random.randint(0, truth_4vecs.shape[0], ngen_samples)
+    gen_input = np.c_[noise, truth_4vecs[rand_rows]]
+    print(gen_input.shape)
+    gen_output = model.predict(gen_input)
+    print(gen_output.shape)
+    masses=M(gen_output[:,0],gen_output[:,1],gen_output[:,2],gen_output[:,3],gen_output[:,4],gen_output[:,5],gen_output[:,6],gen_output[:,7])
 
-  print(masses)
+    print(masses)
 
-  print("mean: %s, std: %s " % (masses.mean(), masses.std()))
+    print("mean: %s, std: %s " % (masses.mean(), masses.std()))
 
-  h1 = r.TH1F("h1","masses",60,60,120)
-  #plu.fill_fast(h1, masses)
-  for val in masses: h1.Fill(val)
-  
-  h1.Scale(1./h1.Integral())
+    h1 = r.TH1F("h1","masses",60,60,120)
+    #plu.fill_fast(h1, masses)
+    for val in masses: h1.Fill(val)
+    
+    h1.Scale(1./h1.Integral())
 
-  ply.plot_hist(
-      bgs=[h1,h2],
-      legend_labels = ["pred", "truth"],
-      options = {
-  	"do_stack": False,
-  	"yaxis_log": False,
-  	#"ratio_numden_indices": [1,0],
-  	"output_name": "%s/masses_epoch_%s.pdf" % (saved_model[:saved_model.index("gen_")], saved_model[saved_model.index("gen_")+4:saved_model.index(".weights")]),
-  	}
-      )
+    ply.plot_hist(
+        bgs=[h1,h2],
+        legend_labels = ["pred", "truth"],
+        options = {
+    	"do_stack": False,
+    	"yaxis_log": False,
+    	#"ratio_numden_indices": [1,0],
+    	"output_name": "%s/masses_epoch_%s.pdf" % (saved_model[:saved_model.index("gen_")], saved_model[saved_model.index("gen_")+4:saved_model.index(".weights")]),
+    	}
+        )
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(add_help=False)
+  parser.add_argument("--tag", "-t", help="Tag for the run", type=str)
+  parser.add_argument("--dir", "-d", help="Dir to make plots from, overrules tag, used when you want to plot an older model of a tag", type=str)
+  parser.add_argument("--ngen", "-n", help="Number of sample points to generate per model", type=int)
+  args = parser.parse_args()
+
+  model_name = None
+  model_dir = None
+
+  if args.dir:
+    model_dir=args.dir
+  else:
+    if args.tag:
+      tag=args.tag
+      model_name = sorted(filter(lambda x: ("model_" in x) and (tag in x), os.listdir("model/")))[0]
+    else:
+      model_name = sorted(filter(lambda x: "model_" in x, os.listdir("model/")))[0]
+    model_dir="model/"+model_name+"/"
+
+  if args.ngen:
+    ngen=args.ngen
+  else:
+    ngen=50000
+
+
+  print("Making plots for model in directory: %s" % model_dir)
+  generatePlots(model_dir, ngen)
