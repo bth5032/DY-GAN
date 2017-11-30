f4f0b2715dd3e560beaad561f422edf183fc9053
Nick Amin <amin.nj@gmail.com>
2017-11-30 17:17:08 +0100

    working towards batch hyperparameter scanning

--- gan/gan.py
+++ gan/gan.py
@@ -1,52 +1,77 @@
 # from __future__ import print_function
 
-from keras.datasets import mnist
-from keras.layers import Input, Dense, Reshape, Flatten, Dropout
-from keras.layers import BatchNormalization, Activation, ZeroPadding2D
+import os
+import tensorflow as tf
+if "USE_ONE_CPU" in os.environ:
+    print ">>> using one CPU"
+    config = tf.ConfigProto(
+          intra_op_parallelism_threads=1,
+          inter_op_parallelism_threads=1)
+else:
+    print ">>> possibly using many CPUs"
+    config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
+
+import keras.backend.tensorflow_backend as K
+from keras.layers import Input, Dense, Reshape, Flatten, Dropout, Lambda, BatchNormalization
 from keras.layers.advanced_activations import LeakyReLU
-from keras.layers.convolutional import UpSampling2D, Conv2D
 from keras.models import Sequential, Model, load_model
 from keras.optimizers import Adam
-
-from keras.callbacks import TensorBoard
-
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import *
-
-
-from test.scaler import SymLogScaler
-
-import pickle
-import sys
-sys.path.append("../")
-
-import plottery.plottery as ply
-import plottery.utils as plu
-import matplotlib.pyplot as plt
-import watch
-
-import ROOT as r
-from physicsfuncs import M
-
-import sys
-import os
-
 import numpy as np
 
-output_shape = (8,)
-input_shape = (8,)
+import argparse
+import time
+import pickle
+import sys
+import os
+sys.path.append("../")
 
-d_epochinfo = {}
+import watch
+from physicsfuncs import Minv, get_metrics, cartesian_to_ptetaphi
+
+
 # ss = MinMaxScaler(feature_range=(-1.,1.))
 # ss = RobustScaler()
 # ss = SymLogScaler()
+# ss = SigmoidScaler()
+# ss = SymLogScaler()
 # ss = StandardScaler()
+# ss1 = StandardScaler()
+# ss2 = MinMaxScaler(feature_range=(-1.,1.))
+
 
 class GAN():
-    def __init__(self):
-        self.img_rows = 28 
-        self.img_cols = 28
-        self.channels = 1
+    def __init__(self, args):
+
+        self.args = args
+
+        self.tag = args.tag
+        self.input_file = str(args.input_file)
+        self.noise_shape = (int(args.noise_size),)
+        self.output_shape = (int(args.output_size),)
+        self.noise_type = int(args.noise_type)
+        self.ntest_samples = int(args.ntest_samples)
+        self.nepochs_dump_pred_metrics = int(args.nepochs_dump_pred_metrics)
+        self.nepochs_dump_history = int(args.nepochs_dump_history)
+        self.nepochs_dump_models = int(args.nepochs_dump_models)
+        self.nepochs_max = int(args.nepochs_max)
+        self.batch_size = int(args.batch_size)
+        self.do_concatenate_disc = args.do_concatenate_disc
+        self.do_concatenate_gen = args.do_concatenate_gen
+        self.do_batch_normalization_disc = args.do_batch_normalization_disc
+        self.do_batch_normalization_gen = args.do_batch_normalization_gen
+        self.do_soft_labels = args.do_soft_labels
+        self.do_noisy_labels = args.do_noisy_labels
+        self.nepochs_decay_noisy_labels = int(args.nepochs_decay_noisy_labels)
+        self.use_ptetaphi_additionally = args.use_ptetaphi_additionally
+
+        os.system("mkdir -p progress/{}/".format(self.tag))
+        os.system("cp gan.py progress/{}/".format(self.tag))
+
+        self.data_ref = None
+        self.d_epochinfo = {}
 
         # optimizer = Adam(0.0002, 0.5)
         optimizer_d = "adadelta"
@@ -65,7 +90,7 @@ class GAN():
         self.generator.compile(loss='binary_crossentropy', optimizer=optimizer_g)
 
         # The generator takes noise as input and generated imgs
-        z = Input(shape=input_shape)
+        z = Input(shape=self.noise_shape)
         img = self.generator(z)
 
         # For the combined model we will only train the generator
@@ -83,10 +108,16 @@ class GAN():
 
         model = Sequential()
 
-        model.add(Dense(64, input_shape=input_shape))
+        model.add(Dense(64, input_shape=self.noise_shape))
+        if self.do_batch_normalization_gen:
+            model.add(BatchNormalization())
+        model.add(LeakyReLU(alpha=0.2))
+        if self.do_concatenate_gen:
+            model.add(Lambda(lambda x: K.concatenate([x*x,x])))
+            model.add(LeakyReLU(alpha=0.2))
         model.add(Dense(128))
         model.add(LeakyReLU(alpha=0.2))
-        model.add(Dense(256))
+        model.add(Dense(128))
         model.add(LeakyReLU(alpha=0.2))
         model.add(Dense(128))
         model.add(LeakyReLU(alpha=0.2))
@@ -94,11 +125,12 @@ class GAN():
         model.add(LeakyReLU(alpha=0.2))
         model.add(Dense(32))
         model.add(LeakyReLU(alpha=0.2))
-        model.add(Dense(input_shape[0]))
+        model.add(Dense(self.output_shape[0]))
+        # model.add(Dense(8,activation="tanh"))
 
         model.summary()
 
-        noise = Input(shape=input_shape)
+        noise = Input(shape=self.noise_shape)
         img = model(noise)
 
         return Model(noise, img)
@@ -107,7 +139,21 @@ class GAN():
 
         model = Sequential()
 
-        model.add(Dense(256,input_shape=output_shape))
+        #model.add(Dense(128,activation="relu",input_shape=gen_output_shape))
+        model.add(Dense(128,input_shape=self.output_shape))
+        if self.do_batch_normalization_disc:
+            model.add(BatchNormalization())
+        model.add(LeakyReLU(alpha=0.2))
+        if self.do_concatenate_disc:
+            model.add(Lambda(lambda x: K.concatenate([x*x,x])))
+            model.add(LeakyReLU(alpha=0.2))
+        model.add(Dense(128))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dense(128))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dense(128))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dense(128))
         model.add(LeakyReLU(alpha=0.2))
         model.add(Dense(128))
         model.add(LeakyReLU(alpha=0.2))
@@ -122,23 +168,43 @@ class GAN():
         model.add(Dense(1,activation='sigmoid'))
         model.summary()
 
-        img = Input(shape=output_shape)
+        img = Input(shape=self.output_shape)
         validity = model(img)
 
         return Model(img, validity)
 
-    def train(self, epochs, batch_size=128, tag=""):
+    def train(self):
 
         # data = np.loadtxt(open("dy_mm_events_line.input", "r"), delimiter=",", skiprows=1)
 
-        data = np.load("data_xyz.npy")
+        # data = np.load("data_xyz.npy")
+        data = np.load(self.input_file)
         X_train = data[:,range(1,1+8)]
-        invmass_data = data[:,0]
+
+        if self.use_ptetaphi_additionally:
+            X_train = np.c_[X_train, cartesian_to_ptetaphi(X_train)]
+
+        self.data_ref = X_train[:self.ntest_samples]
+
+        # data = np.load("data_xyz.npy")
+        # cartesian = data[:,range(1,1+8)]
+        # ptetaphi = cartesian_to_ptetaphi(cartesian)
+        # X_train = np.c_[cartesian, ptetaphi]
+        # self.data_ref = X_train[:ntest_samples][:,range(1,1+8)]
+
+        # data = np.load("../delphes/data_Nov10.npa")
+        # X_train = data.view((np.float32, len(data.dtype.names)))[:,range(1,22)]
+        # self.data_ref = X_train[:ntest_samples][:,range(1,1+8)]
 
         # # NOTE. StandardScaler should be fit on training set
         # # and applied the same to train and test, otherwise we 
         # # introduce a bias
+        # ss1.fit(X_train)
+        # X_train = ss1.transform(X_train).astype(np.float32)
+        # ss2.fit(X_train)
+        # X_train = ss2.transform(X_train).astype(np.float32)
         # ss.fit(X_train)
+        # X_train = ss.transform(X_train).astype(np.float32)
 
         # import pickle
         # scalerfile = 'scaler.sav'
@@ -166,9 +232,9 @@ class GAN():
         #         ]
         # invmass_data = data["mll"]
 
-        half_batch = int(batch_size / 2)
+        half_batch = int(self.batch_size / 2)
 
-        for epoch in range(epochs):
+        for epoch in range(self.nepochs_max):
 
             # ---------------------
             #  Train Discriminator
@@ -178,25 +244,62 @@ class GAN():
             idx = np.random.randint(0, X_train.shape[0], half_batch)
             imgs = X_train[idx]
 
-            noise_half = np.random.normal(0, 1, (half_batch, input_shape[0]))
-            noise_full = np.random.normal(0, 1, (batch_size, input_shape[0]))
+            if self.noise_type == 1: # nominal
+                noise_half = np.random.normal(0, 1, (half_batch, self.noise_shape[0]))
+                noise_full = np.random.normal(0, 1, (self.batch_size, self.noise_shape[0]))
 
-            # noise_half = np.random.uniform(-1, 1, (half_batch, input_shape[0]))
-            # noise_full = np.random.uniform(-1, 1, (batch_size, input_shape[0]))
+                if epoch % self.nepochs_dump_pred_metrics == 0 and epoch > 0:
+                    noise_test = np.random.normal(0, 1, (self.ntest_samples,self.noise_shape[0]))
+
+            elif self.noise_type == 2: # random soup, 4,2,2 have to be modified to sum to noise_shape[0]
+                ngaus = self.noise_shape[0] // 2
+                nflat = (self.noise_shape[0] - ngaus) // 2
+                nexpo = self.noise_shape[0] - nflat - ngaus
+                noise_gaus = np.random.normal( 0, 1, (half_batch+self.batch_size, ngaus))
+                noise_flat = np.random.uniform(-1, 1, (half_batch+self.batch_size, nflat))
+                noise_expo = np.random.exponential( 1,    (half_batch+self.batch_size, nexpo))
+                noise = np.c_[ noise_gaus,noise_flat,noise_expo ]
+                noise_half = noise[:half_batch]
+                noise_full = noise[-self.batch_size:]
+
+                if epoch % self.nepochs_dump_pred_metrics == 0 and epoch > 0:
+                    noise_gaus = np.random.normal( 0, 1, (self.ntest_samples, ngaus))
+                    noise_flat = np.random.uniform(-1, 1, (self.ntest_samples, nflat))
+                    noise_expo = np.random.exponential( 1,    (self.ntest_samples, nexpo))
+                    noise_test = np.c_[ noise_gaus,noise_flat,noise_expo ]
+
+            elif self.noise_type == 3: # truth conditioned?
+                noise_half = np.c_[ 
+                        X_train[np.random.randint(0, X_train.shape[0], half_batch)], 
+                        np.random.normal(0, 1, (half_batch,self.noise_shape[0]-X_train.shape[1]))
+                        ]
+                noise_full = np.c_[ 
+                        X_train[np.random.randint(0, X_train.shape[0], self.batch_size)], 
+                        np.random.normal(0, 1, (self.batch_size,self.noise_shape[0]-X_train.shape[1]))
+                        ]
+
+                if epoch % self.nepochs_dump_pred_metrics == 0 and epoch > 0:
+                    noise_test = np.c_[ 
+                            X_train[np.random.randint(0, X_train.shape[0], self.ntest_samples)], 
+                            np.random.normal(0, 1, (self.ntest_samples,self.noise_shape[0]-X_train.shape[1]))
+                            ]
 
             # Generate a half batch of new images
             gen_imgs = self.generator.predict(noise_half)
 
             # Train the discriminator
-
             ones = np.ones((half_batch, 1))
             zeros = np.zeros((half_batch, 1))
 
-            frac = 0.3*np.exp(-epoch/3000.)
-            if frac > 0.005:
-                # print np.random.randint(0, len(ones), int(frac*len(ones)))
-                ones[np.random.randint(0, len(ones), int(frac*len(ones)))] = 0
-                zeros[np.random.randint(0, len(zeros), int(frac*len(zeros)))] = 1
+            if self.do_soft_labels:
+                ones *= 0.9
+
+            if self.do_noisy_labels:
+                frac = 0.3*np.exp(-epoch/self.nepochs_decay_noisy_labels)
+                if frac > 0.005:
+                    # print np.random.randint(0, len(ones), int(frac*len(ones)))
+                    ones[np.random.randint(0, len(ones), int(frac*len(ones)))] = 0
+                    zeros[np.random.randint(0, len(zeros), int(frac*len(zeros)))] = 1
 
             d_loss_real = self.discriminator.train_on_batch(imgs, ones)
             d_loss_fake = self.discriminator.train_on_batch(gen_imgs, zeros)
@@ -210,7 +313,7 @@ class GAN():
 
             # The generator wants the discriminator to label the generated samples
             # as valid (ones)
-            valid_y = np.array([1] * batch_size)
+            valid_y = np.array([1] * self.batch_size)
 
             # Train the generator
             g_loss = self.combined.train_on_batch(noise_full, valid_y)
@@ -218,55 +321,79 @@ class GAN():
             # Plot the progress
             print ("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100*d_loss[1], g_loss))
 
-            if epoch % 100 == 0:
+            if epoch % self.nepochs_dump_pred_metrics == 0 and epoch > 0:
 
-                noise = np.random.normal(0, 1, (5000,input_shape[0]))
-                gen_imgs = self.generator.predict(noise)
+                gen_imgs = self.generator.predict(noise_test)
+
                 # gen_imgs = ss.inverse_transform(gen_imgs)
-                masses=M(gen_imgs[:,0],gen_imgs[:,1],gen_imgs[:,2],gen_imgs[:,3],gen_imgs[:,4],gen_imgs[:,5],gen_imgs[:,6],gen_imgs[:,7])
+                # gen_imgs = ss2.inverse_transform(gen_imgs)
+                # gen_imgs = ss1.inverse_transform(gen_imgs)
+
+                masses = Minv(gen_imgs)
+                try:
+                    metric1, metric2 = get_metrics(gen_imgs, self.data_ref)
+                except:
+                    metric1, metric2 = 0., 0.
                 masses = masses[np.isfinite(masses)]
                 print masses.mean(), masses.std()
 
-                if "epoch" not in d_epochinfo: 
-                    d_epochinfo["epoch"] = []
-                    d_epochinfo["d_acc"] = []
-                    d_epochinfo["d_loss"] = []
-                    d_epochinfo["g_loss"] = []
-                    d_epochinfo["mass_mu"] = []
-                    d_epochinfo["mass_sig"] = []
+                if "epoch" not in self.d_epochinfo: 
+                    self.d_epochinfo["epoch"] = []
+                    self.d_epochinfo["d_acc"] = []
+                    self.d_epochinfo["d_loss"] = []
+                    self.d_epochinfo["g_loss"] = []
+                    self.d_epochinfo["mass_mu"] = []
+                    self.d_epochinfo["mass_sig"] = []
+                    self.d_epochinfo["metric1"] = []
+                    self.d_epochinfo["metric2"] = []
+                    self.d_epochinfo["time"] = []
+                    self.d_epochinfo["args"] = vars(self.args)
                 else:
-                    d_epochinfo["epoch"].append(epoch)
-                    d_epochinfo["d_acc"].append(100*d_loss[1])
-                    d_epochinfo["d_loss"].append(d_loss[0])
-                    d_epochinfo["g_loss"].append(g_loss)
-                    d_epochinfo["mass_mu"].append(masses.mean())
-                    d_epochinfo["mass_sig"].append(masses.std())
+                    self.d_epochinfo["epoch"].append(epoch)
+                    self.d_epochinfo["d_acc"].append(100*d_loss[1])
+                    self.d_epochinfo["d_loss"].append(d_loss[0])
+                    self.d_epochinfo["g_loss"].append(g_loss)
+                    self.d_epochinfo["mass_mu"].append(masses.mean())
+                    self.d_epochinfo["mass_sig"].append(masses.std())
+                    self.d_epochinfo["metric1"].append(metric1)
+                    self.d_epochinfo["time"].append(time.time())
 
-                np.save("progress/{}/pred_{}.npy".format(tag,epoch), gen_imgs)
-                pickle.dump(d_epochinfo, open("progress/{}/history.pkl".format(tag),'w'))
+                np.save("progress/{}/pred_{}.npy".format(self.tag,epoch), gen_imgs)
+                pickle.dump(self.d_epochinfo, open("progress/{}/history.pkl".format(self.tag),'w'))
 
-            if epoch % 200 == 0:
-                fname = "progress/{}/history.pkl".format(tag)
+            if epoch % self.nepochs_dump_history == 0 and epoch > 0:
+                fname = "progress/{}/history.pkl".format(self.tag)
                 watch.update(fname)
 
-
-
-            if epoch % 10000 == 0: 
-                noise = np.random.normal(0, 1, (10000,input_shape[0]))
-                gen_imgs = self.generator.predict(noise)
-                masses=M(gen_imgs[:,0],gen_imgs[:,1],gen_imgs[:,2],gen_imgs[:,3],gen_imgs[:,4],gen_imgs[:,5],gen_imgs[:,6],gen_imgs[:,7])
-                print("mean: %s, std: %s " % (masses.mean(), masses.std()))
-                self.generator.save("progress/{}/gen_{}.weights".format(tag,epoch))
+            if epoch % self.nepochs_dump_models == 0 and epoch > 0: 
+                self.discriminator.save("progress/{}/disc_{}.weights".format(self.tag,epoch))
+                self.generator.save("progress/{}/gen_{}.weights".format(self.tag,epoch))
 
 
 if __name__ == '__main__':
-    # tag = "v1noise2"
-    # tag = "v1noise1"
-    # tag = "vdecaynoise"
-    tag = "vdecayminmax"
-    os.system("mkdir -p progress/{}/".format(tag))
-    os.system("cp gan.py progress/{}/".format(tag))
 
-    gan = GAN()
-    # gan.train(epochs=100002, batch_size=10000, tag=tag)
-    gan.train(epochs=100002, batch_size=5000, tag=tag)
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("tag", help="run tag for bookkeping in progress dir")
+    parser.add_argument("--input_file", help="input numpy file", default="data_xyz.npy")
+    parser.add_argument("--output_size", help="size of an element to generate", default=8)
+    parser.add_argument("--noise_size", help="size of noise for generator", default=8)
+    parser.add_argument("--noise_type", help="noise type (1: gaussians, 2: mixture, 3: truth-conditioned)", default=1)
+    parser.add_argument("--ntest_samples", help="number of test samples to dump out", default=5000)
+    parser.add_argument("--nepochs_dump_pred_metrics", help="after how many epochs to dump test samples and metrics", default=250)
+    parser.add_argument("--nepochs_dump_history", help="after how many epochs to dump history pickle and update web interface", default=500)
+    parser.add_argument("--nepochs_dump_models", help="after how many epochs to dump disc and gen models", default=5000)
+    parser.add_argument("--nepochs_max", help="max number of epochs to run", default=25000)
+    parser.add_argument("--batch_size", help="batch size", default=200)
+    parser.add_argument("--do_concatenate_disc", help="concatenation layer of x^2 for discriminator", action="store_true")
+    parser.add_argument("--do_concatenate_gen", help="concatenation layer of x^2 for generator", action="store_true")
+    parser.add_argument("--do_batch_normalization_disc", help="batch normalization for discriminator", action="store_true")
+    parser.add_argument("--do_batch_normalization_gen", help="batch normalization for generator", action="store_true")
+    parser.add_argument("--do_soft_labels", help="use 0.9 instead of 1.0 for target values in discriminator training", action="store_true")
+    parser.add_argument("--do_noisy_labels", help="flip target values in discriminator training randomly", action="store_true")
+    parser.add_argument("--nepochs_decay_noisy_labels", help="characteristic decay time in nepochs for noisy label flipping", default=3000)
+    parser.add_argument("--use_ptetaphi_additionally", help="instead of 8 cartesian inputs, use 8 cartesian and 8 e pt eta phi (modify output_size to be 16 then)", action="store_true")
+    args = parser.parse_args()
+
+    gan = GAN(args)
+    gan.train()
--- gan/physicsfuncs.py
+++ gan/physicsfuncs.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.stats import moment
 
 def M_ptetaphi(E1, pt1, eta1, phi1, E2, pt2, eta2, phi2):
   """Outputs the Mass of a two particle system when given a list of E1, pt1, eta1, phi1, E2, pt2, eta2, phi2"""
@@ -25,13 +26,6 @@ def Minv(eight_cartesian_cols):
   M2 -= (eight_cartesian_cols[:,3]+eight_cartesian_cols[:,7])**2
   return np.sqrt(M2)
 
-def get_phis(eight_cartesian_cols):
-    """Get vector of phis for both leptons"""
-    px1 = eight_cartesian_cols[:,1]
-    px2 = eight_cartesian_cols[:,5]
-    py1 = eight_cartesian_cols[:,2]
-    py2 = eight_cartesian_cols[:,6]
-    return  np.r_[np.arctan2(py1,px1),np.arctan2(py2,px2)]
 
 def get_pxs(eight_cartesian_cols):
     return np.r_[ eight_cartesian_cols[:,1], eight_cartesian_cols[:,5] ]
@@ -45,6 +39,61 @@ def get_pts(eight_cartesian_cols):
     px2 = eight_cartesian_cols[:,5]
     py2 = eight_cartesian_cols[:,6]
     return np.r_[ np.sqrt(px1**2+py1**2), np.sqrt(px2**2+py2**2) ]
+
+def cartesian_to_ptetaphi(eight_cartesian_cols):
+    """
+    Takes 8 columns as cartesian
+    e px py pz e px py pz
+    and converts to
+    e pt eta phi e pt eta phi
+    """
+    e1 =  eight_cartesian_cols[:,0]
+    e2 =  eight_cartesian_cols[:,4]
+    px1 = eight_cartesian_cols[:,1]
+    px2 = eight_cartesian_cols[:,5]
+    py1 = eight_cartesian_cols[:,2]
+    py2 = eight_cartesian_cols[:,6]
+    pz1 = eight_cartesian_cols[:,3]
+    pz2 = eight_cartesian_cols[:,7]
+    p1 = np.sqrt(px1**2+py1**2+pz1**2)
+    p2 = np.sqrt(px2**2+py2**2+pz2**2)
+    pt1 = np.sqrt(px1**2+py1**2)
+    pt2 = np.sqrt(px2**2+py2**2)
+    phi1 = np.arctan2(py1,px1)
+    phi2 = np.arctan2(py2,px2)
+    eta1 = np.arctanh(pz1/p1)
+    eta2 = np.arctanh(pz2/p2)
+    return np.c_[e1,pt1,eta1,phi1,e2,pt2,eta2,phi2]
+
+def ptetaphi_to_cartesian(eight_eptetaphi_cols):
+    """
+    Takes 8 columns as e pt eta phi e pt eta phi
+    and converts to
+    e px py pz e px py pz
+    """
+    e1 =  eight_eptetaphi_cols[:,0]
+    e2 =  eight_eptetaphi_cols[:,4]
+    pt1 =  eight_eptetaphi_cols[:,1]
+    pt2 =  eight_eptetaphi_cols[:,5]
+    eta1 =  eight_eptetaphi_cols[:,2]
+    eta2 =  eight_eptetaphi_cols[:,6]
+    phi1 =  eight_eptetaphi_cols[:,3]
+    phi2 =  eight_eptetaphi_cols[:,7]
+    px1 = np.abs(pt1)*np.cos(phi1)
+    px2 = np.abs(pt2)*np.cos(phi2)
+    py1 = np.abs(pt1)*np.sin(phi1)
+    py2 = np.abs(pt2)*np.sin(phi2)
+    pz1 = np.abs(pt1)/np.tan(2.0*np.arctan(np.exp(-1.*eta1)))
+    pz2 = np.abs(pt2)/np.tan(2.0*np.arctan(np.exp(-1.*eta2)))
+    return np.c_[e1,px1,py1,pz1,e2,px2,py2,pz2]
+
+def get_phis(eight_cartesian_cols):
+    """Get vector of phis for both leptons"""
+    px1 = eight_cartesian_cols[:,1]
+    px2 = eight_cartesian_cols[:,5]
+    py1 = eight_cartesian_cols[:,2]
+    py2 = eight_cartesian_cols[:,6]
+    return  np.r_[np.arctan2(py1,px1),np.arctan2(py2,px2)]
 
 def get_dphis(eight_cartesian_cols):
     """Get vector of dphis between both leptons"""
@@ -90,46 +139,49 @@ def Z_pT(eight_cartesian_cols):
     pysum = eight_cartesian_cols[:,2]+eight_cartesian_cols[:,6]
     return np.sqrt(pxsum**2+pysum**2)
     
-def cartesian_to_ptetaphi(eight_cartesian_cols):
-    """
-    Takes 8 columns as cartesian 
-    e px py pz e px py pz
-    and converts to
-    e pt eta phi e pt eta phi
-    """
-    e1 =  eight_cartesian_cols[:,0]
-    e2 =  eight_cartesian_cols[:,4]
-    px1 = eight_cartesian_cols[:,1]
-    px2 = eight_cartesian_cols[:,5]
-    py1 = eight_cartesian_cols[:,2]
-    py2 = eight_cartesian_cols[:,6]
-    pz1 = eight_cartesian_cols[:,3]
-    pz2 = eight_cartesian_cols[:,7]
-    p1 = np.sqrt(px1**2+py1**2+pz1**2)
-    p2 = np.sqrt(px2**2+py2**2+pz2**2)
-    pt1 = np.sqrt(px1**2+py1**2)
-    pt2 = np.sqrt(px2**2+py2**2)
-    phi1 = np.arctan2(py1,px1)
-    phi2 = np.arctan2(py2,px2)
-    eta1 = np.arctanh(pz1/p1)
-    eta2 = np.arctanh(pz2/p2)
-    return np.c_[e1,pt1,eta1,phi1,e2,pt2,eta2,phi2]
 
-def get_redchi2(ref, target):
+def get_momentmetric(obs,exp):
     """
-    Given reference values and target values, compute
-    the reduced chi2 of the binned histogram of both
+    return a metric of agreement based on the moments of the input distributions
     """
-    hmin = ref.mean()-2.*ref.std()
-    hmax = ref.mean()+2.*ref.std()
-    histref = 1.0*np.histogram(ref, bins=100, range=(hmin,hmax))[0]
-    histtarget = 1.0*np.histogram(target, bins=100, range=(hmin,hmax))[0]
-    # print histref
-    # print histref.sum()
-    histtarget *= histref.sum()/histtarget.sum()
-    chi2withnan = ((histtarget-histref)**2. / histref)
-    chi2nonan = chi2withnan[np.isfinite(chi2withnan)]
-    chi2 = np.sqrt(np.sum(chi2nonan))
-    ndof = len(chi2nonan)-1
-    return chi2/ndof
+    obs[~np.isfinite(obs)] = 0
+    return np.abs(moment(obs,2)-moment(exp,2))/1e6 + \
+           np.abs(moment(obs,3)-moment(exp,3))/1e7 + \
+           np.abs(moment(obs,4)-moment(exp,4))/1e8
 
+def get_redchi2(obs,exp):
+    """
+    return reduced chi2 (chi2/ndof) for observed and expected values.
+    both inputs must have the same length (i.e., their integral must
+    be the same, as no normalization is done here).
+    """
+    obs[~np.isfinite(obs)] = 0
+    counts1, bins1 = np.histogram(obs)
+    counts2, bins2 = np.histogram(exp, bins1)
+    tosum = (1.0*(counts1-counts2)**2 / counts2)
+    return np.sum(tosum[np.isfinite(tosum)])/np.sum(np.isfinite(tosum))
+
+def get_quantities(data):
+    return {
+            "mll": Minv(data),
+            "ZpZ": Z_pZ(data),
+            "ZpT": Z_pT(data),
+            "px": get_pxs(data),
+            "py": get_pys(data),
+            "dphi": get_dphis(data),
+            "pt": get_pts(data),
+            "eta": get_etas(data),
+            "deta": get_detas(data),
+            }
+
+def get_metrics(data_obs, data_exp):
+    d_obs = get_quantities(data_obs)
+    d_exp = get_quantities(data_exp)
+    metric1, metric2 = 0., 0.
+    for key in d_obs:
+        metric1 += get_momentmetric(d_obs[key], d_exp[key])
+        metric2 += get_redchi2(d_obs[key], d_exp[key])
+    metric1 /= len(d_obs.keys())
+    metric2 /= len(d_obs.keys())
+    metric2 = np.log(metric2)
+    return metric1, metric2
