47d2ae2910fb344f7e410326f86ca890e64868b8
Bobak Hashemi <bth5032@gmail.com>
2018-05-16 14:11:19 -0700

    added some statistical tools in gan_skip_2

--- gan/notebooks/make_gif.sh
+++ gan/notebooks/make_gif.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-convert -delay 30 -loop 0 `ls -1 $1/*.png | python natsort.py ` animation.gif
+convert -delay 30 -loop 0 `ls -1 $1/*.png | python natsort.py ` $1/animation.gif
 echo "made animation.gif"
